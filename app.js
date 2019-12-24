d4149c72984792a3d12c0b7eab89247f5c029a16
pavel.parfentii <pavelprfnt@gmail.com>
2019-12-24 18:20:20 +0200

    Added burger button

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav__links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    })
+}
+
+navSlide();
