fb94c719dba0b2fd62a2c57ec1be12de1e3b45b5
pavel.parfentii <pavelprfnt@gmail.com>
2019-12-26 18:43:24 +0200

    added page 7,8

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-'use strict'
+"use strict";
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav__links');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__links");
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    })
-}
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
 
 navSlide();
